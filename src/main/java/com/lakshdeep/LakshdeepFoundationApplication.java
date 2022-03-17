5a409cb60f9bf6c580d2e51e98fb57ea962cba97
DESKTOP-MCRF7H7\DELL <sachinsingh959414@gmail.com>
2022-03-17 20:21:13 +0530

    project

---
+++ src/main/java/com/lakshdeep/LakshdeepFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.lakshdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakshdeepFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakshdeepFoundationApplication.class, args);
+	}
+
+}
